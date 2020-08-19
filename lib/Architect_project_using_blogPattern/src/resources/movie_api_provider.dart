a429307313c2f6983119365243247984cc5a56d0
Abdou Aboubacar <abdouabarchia@gmail.com>
2020-08-19 18:43:31 +0100

    Integret Firebase And Architect project using bloc pattern Part 2

--- lib/Architect_project_using_blogPattern/src/resources/movie_api_provider.dart
+++ lib/Architect_project_using_blogPattern/src/resources/movie_api_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter_all/Architect_project_using_blogPattern/src/models/trailer_model.dart';
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
@@ -6,18 +7,27 @@ import '../models/item_model.dart';
 class MovieApiProvider {
   Client client = Client();
   final _apiKey = '2bc3966416b1ba90b9bf14cbe985d651';
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
   Future<ItemModel> fetchMovieList() async {
-    print("entered");
-    final response = await client
-        .get("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
-    print(response.body.toString());
+    final response = await client.get("$_baseUrl/popular?api_key=$_apiKey");
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
       return ItemModel.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response =
+        await client.get("$_baseUrl/$movieId/videos?api_key=$_apiKey");
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
     }
   }
 }
--- lib/Architect_project_using_blogPattern/src/ui/movie_list.dart
+++ lib/Architect_project_using_blogPattern/src/ui/movie_list.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
+import 'movie_detail.dart';
+import '../blocs/movie_detail_bloc_provider.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -28,13 +48,37 @@ class MovieList extends StatelessWidget {
     return GridView.builder(
         itemCount: snapshot.data.results.length,
         gridDelegate:
-            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data
-                .results[index].poster_path}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: InkResponse(
+              enableFeedback: true,
+              child: Image.network(
+                'https://image.tmdb.org/t/p/w185${snapshot.data
+                    .results[index].poster_path}',
+                fit: BoxFit.cover,
+              ),
+              onTap: () => openDetailPage(snapshot.data, index),
+            ),
           );
         });
+  }
+
+  openDetailPage(ItemModel data, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return MovieDetailBlocProvider(
+          child: MovieDetail(
+            title: data.results[index].title,
+            posterUrl: data.results[index].backdrop_path,
+            description: data.results[index].overview,
+            releaseDate: data.results[index].release_date,
+            voteAverage: data.results[index].vote_average.toString(),
+            movieId: data.results[index].id,
+          ),
+        );
+      }),
+    );
   }
 }
